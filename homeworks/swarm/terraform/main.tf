1c890f06636202be7aae592a34ed10a8b491db0d
Andrey Plekhanov <1989plekhanov@gmail.com>
2024-02-01 10:59:24 +0300

    swarm

---
+++ homeworks/swarm/terraform/main.tf
@@ -0,0 +1,17 @@
+resource "yandex_vpc_network" "network" {
+  name = "swarm-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "swarm_cluster" {
+  source        = "./modules/instance"
+  vpc_subnet_id = yandex_vpc_subnet.subnet.id
+  managers      = 1
+  workers       = 2
+}
---
+++ homeworks/swarm/terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "cloud_id" {
+  description = "default cloud_id"
+  type        = string
+  default     = "YOUR CLOUD ID"
+}
+
+variable "folder_id" {
+  description = "default folder_id"
+  type        = string
+  default     = "YOUR FOLDER ID"
+}
+
+variable "token" {
+  description = "default token"
+  type        = string
+  default     = "YOUR TOKEN"
+}
+
+
+
+
+
+
+variable "zone" {
+  description = "default zone"
+  type        = string
+  default     = "ru-central1-a"
+}
